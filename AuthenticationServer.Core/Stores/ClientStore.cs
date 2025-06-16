2c6b124533839e82a855b8193be78f4aa9ae3de8
Tobias Runge <Tobias.Runge95@gmx.de>
2025-06-16 08:19:56 +0200

    Added ClinetStore and ScopeStore

---
+++ AuthenticationServer.Core/Stores/ClientStore.cs
@@ -0,0 +1,112 @@
+﻿using AuthenticationServer.Database;
+using AuthenticationServer.Database.Models;
+
+namespace AuthenticationServer.Core.Stores;
+
+public interface IClientStore
+{
+    Task AddAsync(Client client);
+    Task AddManyAsync(List<Client> clients);
+    Task RemoveAsync(Client client);
+    Task RemoveManyAsync(List<Client> clients);
+    Task UpdateAsync(Client client);
+    Task UpdateManyAsync(List<Client> clients);
+    Task<Client> GetAsync(Guid id);
+    Task<List<Client>> GetAllAsync();
+    Task<bool> ContainsAtLeastOneValidRedirectUri(Guid id);
+    
+    // --- ClientScope
+    Task AddAsync(ClientScope clientScope);
+    Task AddManyAsync(List<ClientScope> clientScopes);
+    Task RemoveAsync(ClientScope clientScope);
+    Task RemoveManyAsync(List<ClientScope> clientScopes);
+    Task<ClientScope> GetClientScopeAsync(Guid id);
+    Task<List<ClientScope>> GetAllClientScopesAsync();
+}
+
+public class ClientStore : IClientStore
+{
+    private readonly ApplicationDbContext _db;
+
+    public ClientStore(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public Task AddAsync(Client client)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddManyAsync(List<Client> clients)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task RemoveAsync(Client client)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task RemoveManyAsync(List<Client> clients)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(Client client)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateManyAsync(List<Client> clients)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Client> GetAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Client>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> ContainsAtLeastOneValidRedirectUri(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    // --- ClientScope
+    
+    public Task AddAsync(ClientScope clientScope)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddManyAsync(List<ClientScope> clientScopes)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task RemoveAsync(ClientScope clientScope)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task RemoveManyAsync(List<ClientScope> clientScopes)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ClientScope> GetClientScopeAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<ClientScope>> GetAllClientScopesAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ AuthenticationServer.Core/Stores/ScopeStore.cs
@@ -0,0 +1,70 @@
+﻿using AuthenticationServer.Database.Models;
+
+namespace AuthenticationServer.Core.Stores;
+
+public interface IScopeStore
+{
+    Task AddOneAsync(Scope scope);
+    Task AddManyAsync(List<Scope> scopes);
+    Task RemoveOneAsync(Guid id);
+    Task RemoveManyAsync(List<Guid> ids);
+    Task UpdateOneAsync(Scope scope);
+    Task UpdateManyAsync(List<Scope> scopes);
+    Task<Scope> GetOneAsync(Guid id);
+    Task<List<Scope>> GetAllAsync();
+    Task<List<Scope>> GetAllAsync(List<Guid> ids);
+    Task<List<Scope>> GetAllAsync(List<string> names);
+}
+
+public class ScopeStore :  IScopeStore
+{
+    public Task AddOneAsync(Scope scope)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddManyAsync(List<Scope> scopes)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task RemoveOneAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task RemoveManyAsync(List<Guid> ids)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateOneAsync(Scope scope)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateManyAsync(List<Scope> scopes)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Scope> GetOneAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Scope>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Scope>> GetAllAsync(List<Guid> ids)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Scope>> GetAllAsync(List<string> names)
+    {
+        throw new NotImplementedException();
+    }
+}
