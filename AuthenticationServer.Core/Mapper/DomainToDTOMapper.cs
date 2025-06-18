ea12221286485e32620d35399a4e137f2970e10a
Tobias Runge <Tobias.Runge95@gmx.de>
2025-06-18 15:24:20 +0200

    Added a static DTO Mapper Class

---
+++ AuthenticationServer.Core/Mapper/DomainToDTOMapper.cs
@@ -0,0 +1,49 @@
+﻿using AuthenticationServer.Core.DTO.Client;
+using AuthenticationServer.Core.DTO.Scope;
+using AuthenticationServer.Database.Models;
+
+namespace AuthenticationServer.Core.Mapper;
+
+public static class DomainToDTOMapper
+{
+    public static ScopeDTO MapToScopeDTO(this Scope scope)
+    {
+        return new ScopeDTO
+        {
+            Id = scope.Id,
+            Name = scope.Name,
+            Description = scope.Description
+        };
+    }
+
+    public static ClientDTO MapToClientDTO(this Client client)
+    {
+        var scopeDTOs = new List<ScopeDTO>();
+        foreach (ClientScope clientScope in client.ClientScopes)
+        {
+            scopeDTOs.Add(clientScope.Scope.MapToScopeDTO());
+        }
+
+        return new ClientDTO
+        {
+            Id = client.Id,
+            AllowedGrantTypes = client.AllowedGrantTypes,
+            ClientName = client.ClientName,
+            ClientType = client.ClientType,
+            RedirectUris = client.RedirectUris,
+            AllowedScopes = scopeDTOs
+        };
+    }
+
+    public static ShortClientDTO MapToShortClientDTO(this Client client)
+    {
+        return new ShortClientDTO
+        {
+            ClientName = client.ClientName,
+            ClientType = client.ClientType,
+            Id = client.Id,
+            CreatedAt = client.CreatedAt,
+            IsEnabled = client.IsEnabled,
+        };
+    }
+}
