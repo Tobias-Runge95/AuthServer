29f354a21f59592319e869794f527c5a5472e610
Tobias Runge <Tobias.Runge95@gmx.de>
2025-06-20 07:56:23 +0200

    Removed nullable option in AuthorizationCode

--- AuthenticationServer.Database/Models/AuthorizationCode.cs
+++ AuthenticationServer.Database/Models/AuthorizationCode.cs
@@ -5,10 +5,10 @@ public class AuthorizationCode
     public Guid Id { get; set; }
     public string Code { get; set; }                  // zufällig generierter String
     public string RedirectUri { get; set; }
-    public Guid? ClientId { get; set; }
-    public Client? Client { get; set; }
-    public Guid? SubjectId { get; set; }
-    public User? Subject { get; set; }             // User ID (von Identity User)
+    public Guid ClientId { get; set; }
+    public Client Client { get; set; }
+    public Guid SubjectId { get; set; }
+    public User Subject { get; set; }             // User ID (von Identity User)
     public DateTime ExpiresAt { get; set; }
     public ICollection<AuthorizationCodeScope> Scopes { get; set; }
     public List<string> RequestedScopes { get; set; } = new();
