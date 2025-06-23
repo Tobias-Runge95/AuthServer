e0808851d7b37911c0ac42302228067dad63b2a5
Tobias Runge <Tobias.Runge95@gmx.de>
2025-06-23 15:41:44 +0200

    Removed and Added UserStore / UserClientStore

---
+++ AuthenticationServer.Core/Stores/UserClientStore.cs
@@ -0,0 +1,17 @@
+﻿using AuthenticationServer.Core.Manager;
+using AuthenticationServer.Database;
+using AuthenticationServer.Database.Models;
+
+namespace AuthenticationServer.Core.Stores;
+
+public interface IUserClientStore : IBaseStore<UserClient>
+{
+    
+}
+
+public class UserClientStore : BaseStore<UserClient>, IUserClientStore
+{
+    public UserClientStore(ApplicationDbContext db) : base(db)
+    {
+    }
+}
