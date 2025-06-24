85cca5fc2cd3a925ceb479befe52b894bce05641
Tobias Runge <Tobias.Runge95@gmx.de>
2025-06-24 10:20:58 +0200

    Added RoleStore and UserStore as ApplicationUserStore

---
+++ AuthenticationServer.Core/Stores/ApplicationUserStore.cs
@@ -0,0 +1,43 @@
+﻿using AuthenticationServer.Database;
+using AuthenticationServer.Database.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthenticationServer.Core.Stores;
+
+public interface IApplicationUserStore
+{
+    Task<User> GetUserAsync(Guid userId);
+    Task<User> GetUserAsync(string userName);
+    Task<User> GetUserByEmailAsync(string email);
+}
+
+public class ApplicationUserStore : IApplicationUserStore
+{
+    private readonly ApplicationDbContext _context;
+
+    public ApplicationUserStore(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+
+    public Task<User> GetUserAsync(Guid userId)
+    {
+        return _context.User
+            .Where(u => u.Id == userId)
+            .Include(u => u.ContactForClient)
+            .Include(uc => uc.UserClients)
+            .FirstOrDefaultAsync()!;
+    }
+
+    public Task<User> GetUserAsync(string userName)
+    {
+        return _context.User.FirstOrDefaultAsync(u => u.UserName == userName)!;
+    }
+
+    public Task<User> GetUserByEmailAsync(string email)
+    {
+        return _context.User.FirstOrDefaultAsync(u => u.Email == email)!;
+    }
+}
---
+++ AuthenticationServer.Core/Stores/UserRoleStore.cs
@@ -0,0 +1,18 @@
+﻿using AuthenticationServer.Core.Manager;
+using AuthenticationServer.Database;
+using AuthenticationServer.Database.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace AuthenticationServer.Core.Stores;
+
+public interface IUserRoleStore : IBaseStore<UserRole>
+{
+    
+}
+
+public class UserRoleStore : BaseStore<UserRole>, IUserRoleStore
+{
+    public UserRoleStore(ApplicationDbContext db) : base(db)
+    {
+    }
+}
