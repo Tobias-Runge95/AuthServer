9be605eac42b9a660d501b0c52eab29187203ad3
Tobias Runge <Tobias.Runge95@gmx.de>
2025-06-30 10:45:47 +0200

    Added UpdateRequests for Client

---
+++ AuthenticationServer.Core/Request/Client/UpdateClientAllowedGrantTypes.cs
@@ -0,0 +1,7 @@
+﻿namespace AuthenticationServer.Core.Request.Client;
+
+public class UpdateClientAllowedGrantTypes
+{
+    public Guid ClientId { get; set; }
+    public List<string> AllowedGrantTypes { get; set; }
+}
---
+++ AuthenticationServer.Core/Request/Client/UpdateClientRedirectUris.cs
@@ -0,0 +1,7 @@
+﻿namespace AuthenticationServer.Core.Request.Client;
+
+public class UpdateClientRedirectUris
+{
+    public Guid ClientId { get; set; }
+    public List<string> RedirectUris { get; set; }
+}
